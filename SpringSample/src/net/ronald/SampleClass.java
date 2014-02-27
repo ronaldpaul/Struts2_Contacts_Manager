6e6044dcb06cb81ac2f32f4a7585cd254cf9e283
Ronald <pronaldpaul@yahoo.com>
2014-02-27 10:45:43 +0530

    Added new method

--- SpringSample/src/net/ronald/SampleClass.java
+++ SpringSample/src/net/ronald/SampleClass.java
@@ -4,6 +4,11 @@ public class SampleClass {
 
 	public static void main(String[] args) {
 		System.out.println("This is a sample class");
+		SampleClass.method_one();
+	}
+	
+	public static void method_one() {
+		System.out.println("This is method one");
 	}
 
 }
