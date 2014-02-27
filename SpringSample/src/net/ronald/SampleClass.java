8bd0b6c618cb0d0f8a3fcef7a03ae1259c5adfa0
Ronald <pronaldpaul@yahoo.com>
2014-02-27 19:42:20 +0530

    Testing push in VPN with method two

--- SpringSample/src/net/ronald/SampleClass.java
+++ SpringSample/src/net/ronald/SampleClass.java
@@ -10,5 +10,9 @@ public class SampleClass {
 	public static void method_one() {
 		System.out.println("This is method one");
 	}
+	
+	public static void method_two() {
+		System.out.println("This is method two");
+	}
 
 }
